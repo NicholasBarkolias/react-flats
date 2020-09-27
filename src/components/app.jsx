0490e23c8d8bf9fe0cf125fcd3af56db78974f2e
Nicholas Barkolias <nbarkolias@gmail.com>
2020-09-27 20:10:16 +1000

    app jsx

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -1,11 +1,19 @@
 import React, { Component } from "react";
-import Card from "./card";
 
+import Card from "./card";
 import flats from "../data/flats";
+import SimpleMap from "./simple_map";
 
 class App extends Component {
   render() {
-    return <Card />;
+    return (
+      <div>
+        <Card />
+        <div className='map-container'>
+          <SimpleMap />
+        </div>
+      </div>
+    );
   }
 }
 
